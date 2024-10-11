7f45614bf987021600959c1bc27a589dd189dd40
luckfer7 <lucasferreira059@gmail.com>
2024-10-11 11:08:25 -0300

    1

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import GlobalStyle from './GlobalStyle.jsx'
+import App from './App';
+import GlobalStyle from './GlobalStyle';
+import React from 'react';
 
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <GlobalStyle />
     <App />
